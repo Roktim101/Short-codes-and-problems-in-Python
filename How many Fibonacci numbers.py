2fa6dc48737983266529fee5cf65eed65beb5fd0
Roktim <122729971+Roktim101@users.noreply.github.com>
2023-02-28 00:29:50 +0600

    Update and rename How Many Fibonacci Numbers.py to How many Fibonacci numbers.py

---
+++ How many Fibonacci numbers.py
@@ -0,0 +1,11 @@
+try:
+    n = int(input('How many Fibonacci numbers: '))
+    a, b = 0, 1
+    if n == 0:
+        print('Not possible!')
+    else:
+        for i in range(n):
+            print(a, end=' ')
+            a, b = b, a + b
+except ValueError:
+    print('Invalid value!')
