42c00901cdf29de97699b68a98446726252295ef
Roktim <122729971+Roktim101@users.noreply.github.com>
2023-04-12 22:36:09 +0600

    Add files via upload

---
+++ Replacing multiple sub-strings.py
@@ -0,0 +1,9 @@
+string = "5.6.7x2.9.10x2.12.15 "
+
+# Remember to put sub-strings according to their priority in orders.
+replacements = {" ": " and more", ".": ", ", "x": "*"}
+
+for old_substring, new_substring in replacements.items():
+    string = string.replace(old_substring, new_substring)
+
+print(string)
