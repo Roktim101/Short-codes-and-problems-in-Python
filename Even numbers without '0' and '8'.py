b5a4e9dfd8a6431fbdfc5ece2808c53ba3c62e52
Roktim <122729971+Roktim101@users.noreply.github.com>
2023-04-14 22:26:56 +0600

    Update Even numbers without '0' and '8'.py

--- Even numbers without '0' and '8'.py
+++ Even numbers without '0' and '8'.py
@@ -5,3 +5,11 @@ a = int(input('Range : '))
 for i in range(a):
     if i % 2 == 0 and "0" not in str(i) and "8" not in str(i):
         print(i, end=' ')
+
+# another way 
+
+a = int(input('Range: '))
+
+result = [i for i in range(a) if i % 2 == 0 and "0" not in str(i) and "8" not in str(i)]    # used a list comprehension
+
+print(*result)    # used the unpacking operator '*' to print the list elements separated by spaces.
