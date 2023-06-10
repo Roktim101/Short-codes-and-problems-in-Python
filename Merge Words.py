7fd6a9f37fb0949e014b28c51354672eff4bd331
Roktim <122729971+Roktim101@users.noreply.github.com>
2023-06-11 05:46:32 +0600

    Add files via upload

---
+++ Merge Words.py
@@ -0,0 +1,22 @@
+def merge_words(word1, word2):
+    merged = ''
+    length1 = len(word1)
+    length2 = len(word2)
+    max_length = max(length1, length2)
+
+    for i in range(max_length):
+        if i < length1:
+            merged += word1[i]
+        if i < length2:
+            merged += word2[i]
+
+    return merged
+
+
+# example
+
+word_1 = 'abcde'
+word_2 = 'xyz'
+
+result = merge_words(word_1, word_2)
+print(result)
