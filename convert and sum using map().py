f95dcefe0cf914a78ff67ced63fd62b362b6ca48
Roktim <122729971+Roktim101@users.noreply.github.com>
2024-07-02 04:12:35 -0230

    Update and rename convert and sum.py to convert and sum using map().py

--- convert and sum using map().py
+++ convert and sum using map().py
@@ -11,3 +11,12 @@ numeric_values, total_sum = convert_and_sum_mixed_list(mixed)
 
 print("Numeric Values:", numeric_values)
 print("Sum:", total_sum)
+
+
+
+# another example of map()
+
+mixed = [1, "two", 3.0, "four"]
+joined_mixed = " | ".join(map(str, mixed))    # Convert each element in the list to a string
+print(joined_mixed)
+
