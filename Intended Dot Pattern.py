f9bb1ff962424b696d588a96f6308daed96ace35
Roktim <122729971+Roktim101@users.noreply.github.com>
2024-07-02 04:17:04 -0230

    Update Intended Dot Pattern.py

--- Intended Dot Pattern.py
+++ Intended Dot Pattern.py
@@ -1,5 +1,5 @@
-pattern_1 = lambda n: [print(' ' * (10 - x) + '• ' * x) for x in range(1, n + 1)]
-pattern_2 = lambda n: [print(' ' * (10 - x) + '• ' * x) for x in range(n, 0, -1)]
+pattern_1 = lambda n: [print(' ' * (n - x) + '• ' * x) for x in range(1, n + 1)]
+pattern_2 = lambda n: [print(' ' * (n - x) + '• ' * x) for x in range(n, 0, -1)]
 
 pattern_1(10)
 pattern_2(9)
